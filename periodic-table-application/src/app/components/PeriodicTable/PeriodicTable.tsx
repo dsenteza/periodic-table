85d3029e7eda00e46d97d4c19f112ec817697dc5
Dominique Senteza <dsenteza@Dominiques-Air.lan>
2024-03-14 08:28:06 +0100

    trailing untracked files

---
+++ periodic-table-application/src/app/components/PeriodicTable/PeriodicTable.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type PeriodicTableProps = {
+  name: string;
+  image: string;
+  onClick: () => void;
+};
+
+const PeriodicTable: React.FC<PeriodicTableProps> = ({ name, image, onClick }) => {
+  return (
+   <div></div>
+  );
+};
+
+export default PeriodicTable;
--- periodic-table-application/src/app/components/PeriodicTable/index.ts
+++ periodic-table-application/src/app/components/PeriodicTable/index.ts
@@ -1,16 +1,1 @@
-import React from "react";
-import "./Card.css";
-
-type PeriodicTableProps = {
-  name: string;
-  image: string;
-  onClick: () => void;
-};
-
-const PeriodicTable: React.FC<PeriodicTableProps> = ({ name, image, onClick }) => {
-  return (
-   <> </>
-  );
-};
-
-export default PeriodicTable;
+export * from './PeriodicTable';
